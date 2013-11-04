686226bbbbaa6f6c45c90b3531b38373996382cd
Kyle Alonius <k_alonius@hotmail.com>
2013-11-04 13:18:36 -0600

    Created a Tweet model containing string content, and migrated the database to create the tweets database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SingleUserTwitter::Application.config.secret_token = '22195ae1f41c155a3c16a48fd67115c6e8a7670643b96525bdb73440edb266af6650d51316c2ce3f081a31c050a57c72316adbab29e3a89288904a00961fa2fc'
